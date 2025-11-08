851ba6244c0ceb426535c63ef76d29045a7fbd32
Wiktoria Borek <wiktoria.borek@proton.me>
2025-11-08 10:58:59 +0100

    display

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 import pygame
 import time
 import random
+
+WIDTH, HEIGHT = 800, 600
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Rain Dodge")
+
+def main():
+    run = True
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+    
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
