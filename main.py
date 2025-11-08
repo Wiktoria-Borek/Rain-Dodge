46ba612f700e5dd195daae229b54481889605e60
Wiktoria Borek <wiktoria.borek@proton.me>
2025-11-08 11:32:49 +0100

    background

--- main.py
+++ main.py
@@ -3,17 +3,30 @@ import time
 import random
 
 WIDTH, HEIGHT = 800, 600
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+WIN = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
 pygame.display.set_caption("Rain Dodge")
+
+BG_ORIGINAL = pygame.image.load("background.jpg")
+BG = pygame.transform.smoothscale(BG_ORIGINAL, (WIDTH, HEIGHT))
+
+def draw_window():
+    WIN.blit(BG, (0, 0))
+    pygame.display.update()
 
 def main():
     run = True
 
     while run:
+        draw_window()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
+            elif event.type == pygame.VIDEORESIZE:
+                global WIDTH, HEIGHT, WIN, BG
+                WIDTH, HEIGHT = event.w, event.h
+                WIN = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
+                BG = pygame.transform.smoothscale(BG_ORIGINAL, (WIDTH, HEIGHT))
     
     pygame.quit()
 
